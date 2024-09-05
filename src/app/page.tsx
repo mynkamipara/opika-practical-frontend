0404439e593a5932fdd4c19112ffb6849a105f29
Mayank Amipara <mayankamipara2@gmail.com>
2024-09-05 11:33:57 +0530

    add : MUI configure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,36 @@
 import styles from "./page.module.css";
 import Link from "next/link";
+import PersonIcon from '@mui/icons-material/Person';
+import { Button, Box } from '@mui/material';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div>
       <main className={styles.main}>
-        <div className={styles.ctas}>
-          <Link href={'/profile'} className={styles.primary}>User Profile</Link>
-        </div>
+        <Box
+          sx={{
+            height: '100vh',  
+            display: 'flex', 
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Link href={'/profile'}>
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: 'black',
+                color: 'white', 
+                '&:hover': {
+                  backgroundColor: 'gray',
+                },
+              }}
+            >
+              <PersonIcon />
+              User Profile
+            </Button>
+          </Link>
+        </Box>
       </main>
     </div>
   );
